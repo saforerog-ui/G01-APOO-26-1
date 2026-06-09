6c3f3a44ee44eb03c26ad88bebe5c63200e84c21
saforerog-ui <saforerog@ut.edu.co>
2026-06-08 20:09:09 -0500

    Add files via upload

---
+++ tutoria03/src/test/java/com/example/tutoria03/Tutoria03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutoria03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutoria03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
